abdcf02ed8415793efdeb71d3c17cb9b03294db3
Aarushi <aarushijha2505@gmail.com>
2025-09-22 16:05:43 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# SQLite for hackathon (switch to MySQL later if needed)
 SQLALCHEMY_DATABASE_URL = "sqlite:///./safemap.db"
 
 engine = create_engine(
